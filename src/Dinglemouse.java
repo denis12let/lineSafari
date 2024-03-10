1cd34384a9e1b7e42c4f8569b05b87214a5ef776
denis12let <84den84sokol@gmail.com>
2024-03-11 00:37:41 +0300

    complete!

--- src/Dinglemouse.java
+++ src/Dinglemouse.java
@@ -3,15 +3,6 @@ import java.util.Arrays;
 
 public class Dinglemouse {
     public static void main(String[] args) {
-        //План
-        // -- убрать условие return count != 1 ? false : true; и сделать if (count == 0) return false;
-        // -- сделать так: Создать static flag в этом классе. Если возвращаешься по рекурсии от X, то flag = true, иначе - false (но подумать, что в рекурсии после X можно смотреть еще на другие символы и этот true станет false)
-        //1. Считаем кол-во X отдельной функцией и получаем это как массив координат
-        //2. Созддаем элемент класса ArrayList(чтоб добавлять из рекурсии можно было)
-        //3. В методе line(где просто ищем X-ы и обходим их) создаем массив ArrayList-ов, размером с количество X-ов
-        //4. Обходим X-ы -> добавляем при true координату в ArrayList -> делаем distinct этого массива, чтоб удалить повторы ->
-        //-> добавляем эти ArrayList-ы координат в массив ArrayList-ов из метода line
-        //5. Узнаем какой X к кому относится(придумать как)
         System.out.println("Hello world!");
         final char grid[][] = makeGrid(new String[] {
                 "           ",
@@ -24,78 +15,102 @@ public class Dinglemouse {
 
     public static boolean line(final char [][] grid) {
         Pair coordX = new Pair(); //коорды первого найденного X
-//        ArrayList<Pair> sources = sourcesSearch(grid);
-        for (int i = 0; i < grid.length; i++){
+        ArrayList<Boolean> lines = new ArrayList<>();
+         for (int i = 0; i < grid.length; i++){
             for (int j = 0; j < grid[0].length; j++){
                if (grid[i][j] == 'X'){
                    coordX.setX(i);
                    coordX.setY(j);
-
                    boolean result = lineSearch(grid, coordX, new Pair(-1, -1, '0'));
-                   return result;
+                   lines.add(result);
                }
             }
         }
-        return true;
+        if (lines.stream()
+                .filter(el -> el == true)
+                .count() > 0)
+            return true;
+        return false;
     }
 
     public static boolean lineSearch(char [][] grid, Pair currentPos, Pair oldPos){
         int xCoord = currentPos.getX(), yCoord = currentPos.getY(), count = 0;
-        int oldXDiff = oldPos.getX() - xCoord, oldYDiff = oldPos.getY() - yCoord;
+        int oldXDiff = oldPos.getX() - xCoord, oldYDiff = oldPos.getY() - yCoord, flag = 0, check;
         char currentChar = grid[xCoord][yCoord], oldChar = oldPos.getX() != -1 ? oldPos.getC() : 'X';
-        if (currentChar == ' ') return false;
-        else if (currentChar == '-') {
-            if (oldChar == '|') return false;
-            if ((oldChar == '-' || oldChar == '+' || oldChar == 'X') && oldYDiff == 0) return false;
-        }else if (currentChar == '|') {
-            if (oldChar == '-') return false;
-            if ((oldChar == '|' || oldChar == '+' || oldChar == 'X') && oldXDiff == 0) return false;
-        }else if (currentChar == '+') {
-            if (oldChar == '-' && oldYDiff == 0) return false;
-            if (oldChar == '|' && oldXDiff == 0) return false;
-        }else if (currentChar == 'X'){
-            if (oldPos.getX() != -1){
-                if (oldChar == '-' && oldYDiff == 0) return false;
-                if (oldChar == '|' && oldXDiff == 0) return false;
-                if (oldChar == 'X') return true; //Исправить, чтобы при XX было и как новый путь, и как еще поиск дальше
-                return true;
-            }
+
+        //flag - кол-во возможных путей с одной точки
+        //count - кол-во путей, имеющих конец X
+        //check - смотрит можно ли проложить путь через след эл. с помощью функции charsCheck
+        //oldXdiff и oldYDiff - на каком уровне прошлая и нынешняя точки, т. е. 0 0 или 0
+        //                                                                              0
+
+        if (currentChar == 'X' && oldPos.getX() != -1){
+            if (charArrayExamination(grid)) return true;
         }
-        if (yCoord > 0 && oldYDiff != -1){
+
+        if (yCoord > 0 && oldYDiff != -1){ //право
             grid[xCoord][yCoord] = ' ';
             Pair newPos = new Pair(xCoord, yCoord - 1, currentChar);
-            if (currentChar != '+' || oldXDiff != 0){
-                if(lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+            check = charsCheck(currentChar, grid[xCoord][yCoord - 1], 1);
+            if (check == 1){
+                if (!(currentChar == '+' && oldXDiff == 0 && (oldChar == 'X' || oldChar == '-' || oldChar == '+'))) { //условие, чтоб не было +++ или X++ или -++
+                    flag++;
+                    if(lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+                }
+            }else if (check == 2){
+                flag++; count++;
             }
             grid[xCoord][yCoord] = currentChar;
         }
-        if (yCoord < grid[0].length - 1 && oldYDiff != 1){
+
+        if (yCoord < grid[0].length - 1 && oldYDiff != 1){ //лево
             grid[xCoord][yCoord] = ' ';
             Pair newPos = new Pair(xCoord, yCoord + 1, currentChar);
-            if (currentChar != '+' || oldXDiff != 0) {
-                if (lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+            check = charsCheck(currentChar, grid[xCoord][yCoord + 1], 1);
+            if (check == 1){
+                if (!(currentChar == '+' && oldXDiff == 0 && (oldChar == 'X' || oldChar == '-' || oldChar == '+'))) {
+                    flag++;
+                    if(lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+                }
+            }else if (check == 2){
+                flag++; count++;
             }
+
             grid[xCoord][yCoord] = currentChar;
         }
-        if (xCoord > 0 && oldXDiff != -1){
+
+        if (xCoord > 0 && oldXDiff != -1){ //верх
             grid[xCoord][yCoord] = ' ';
             Pair newPos = new Pair(xCoord - 1, yCoord, currentChar);
-            if (currentChar != '+' || oldYDiff != 0) {
-                if(lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+            check = charsCheck(currentChar, grid[xCoord - 1][yCoord], 2);
+            if (check == 1){
+                if (!(currentChar == '+' && oldYDiff == 0 && (oldChar == 'X' || oldChar == '|' || oldChar == '+'))) {
+                    flag++;
+                    if(lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+                }
+            }else if (check == 2){
+                flag++; count++;
             }
             grid[xCoord][yCoord] = currentChar;
         }
-        if (xCoord < grid.length - 1 && oldXDiff != 1){
+
+        if (xCoord < grid.length - 1 && oldXDiff != 1){ //низ
             grid[xCoord][yCoord] = ' ';
             Pair newPos = new Pair(xCoord + 1, yCoord, currentChar);
-            if (currentChar != '+' || oldYDiff != 0) {
-                if(lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+            check = charsCheck(currentChar, grid[xCoord + 1][yCoord], 2);
+            if (check == 1){
+                if (!(currentChar == '+' && oldYDiff == 0 && (oldChar == 'X' || oldChar == '|' || oldChar == '+'))) {
+                    flag++;
+                    if(lineSearch(grid, newPos, new Pair(xCoord, yCoord, currentChar))) count++;
+                }
+            }else if (check == 2){
+                flag++; count++;
             }
             grid[xCoord][yCoord] = currentChar;
         }
-        return count != 1 ? false : true;
-    }
 
+        return count == 1 && flag == 1 ? true : false;
+    }
 
     public static char[][] makeGrid(String[] strings){ //Перевод массива стрингов в чары
         char[][] grid = new char[strings.length][strings[0].length()];
@@ -107,6 +122,23 @@ public class Dinglemouse {
         return grid;
     }
 
+    public static int charsCheck(char charCurrent, char charNext, int direction){
+        int flag = 0;
+        switch (charCurrent){
+            case '-' -> {
+                if ((charNext == '-' || charNext == '+' || charNext == 'X') && direction == 1) flag = 1;
+            }
+            case '|' -> {
+                if ((charNext == '|' || charNext == '+' || charNext == 'X') && direction == 2) flag = 1;
+            }
+            case '+', 'X' -> {
+                if ((charNext == '|' && direction == 2) || (charNext == '-' && direction == 1) || charNext == '+' || charNext == 'X') flag = 1;
+            }
+        }
+        if (charCurrent == 'X' && charNext == 'X') flag = 2;
+        return flag;
+    }
+
     public static boolean charArrayExamination(char[][] grid){
         int count = 0;
         for (char[] item : grid){
@@ -116,28 +148,9 @@ public class Dinglemouse {
         }
         return count == grid.length * grid[0].length - 1;
     }
-
-    public static ArrayList<Pair> sourcesSearch(char[][] grid){
-        ArrayList<Pair> sourcesCoords = new ArrayList<>();
-        for (int i = 0; i < grid.length; i++){
-            for (int j = 0; j < grid[0].length; j++){
-                if (grid[i][j] == 'X') sourcesCoords.add(new Pair(i, j));
-            }
-        }
-        return sourcesCoords;
-    }
-
-    public static int sourceSearch(ArrayList<Pair> sources, Pair currentCoord){
-        for (int i = 0; i < sources.size(); i++){
-            if (sources.get(i).getX() == currentCoord.getX() && sources.get(i).getY() == currentCoord.getY()) return i;
-        }
-        return -1;
-    }
-
 }
 
 class Pair{
-//    static Pair firstX;
     private int x;
     private int y;
     private char c;
@@ -174,8 +187,5 @@ class Pair{
     public char getC() {
         return c;
     }
-
-    public void setC(char c) {
-        this.c = c;
-    }
 }
+
